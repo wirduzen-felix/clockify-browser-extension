659abb0381339b44c0ecec681ce6485404202fe4
Felix von WIRDUZEN <felix@wirduzen.de>
2022-04-07 10:11:02 +0000

    Update config_chrome.rb

--- config_chrome.rb
+++ config_chrome.rb
@@ -3,9 +3,9 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "chrome.dev/styles"
+css_dir = "chrome/styles"
 sass_dir = "sass"
-images_dir = "chrome.dev/assets/images"
+images_dir = "chrome/assets/images"
 images_path = "assets/images"
 javascripts_dir = "javascripts"
 
